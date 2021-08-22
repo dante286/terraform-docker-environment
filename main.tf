113d4a31fb6e4cdcef8736fd1b0c7fc378b7a578
dante286 <matt.nisan@gmail.com>
2021-08-22 08:10:33 -0400

    Initial image and volume creation.  Will dynamically pull new images when sha256 sig changes

--- main.tf
+++ main.tf
@@ -1,1 +1,58 @@
 # Create a series of pods on remote docker host that can be reached individually
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+  host = "ssh://${var.docker_user}@docker.${var.domain_name}:22"
+}
+
+data "docker_registry_image" "nginx_image" {
+  name = "jwilder/nginx-proxy"
+}
+
+resource "docker_image" "rproxy_latest" {
+  name          = data.docker_registry_image.nginx_image.name
+  pull_triggers = [data.docker_registry_image.nginx_image.sha256_digest]
+}
+
+data "docker_registry_image" "jenkins_image" {
+  name = "jenkins/jenkins"
+}
+
+resource "docker_image" "jenkins_latest" {
+  name          = data.docker_registry_image.jenkins_image.name
+  pull_triggers = [data.docker_registry_image.jenkins_image.sha256_digest]
+}
+
+resource "docker_volume" "jenkins_volume" {
+  name = "jenkins_data"
+}
+
+data "docker_registry_image" "transmission_image" {
+  name = "linuxserver/transmission"
+}
+
+resource "docker_image" "transmission_latest" {
+  name          = data.docker_registry_image.transmission_image.name
+  pull_triggers = [data.docker_registry_image.transmission_image.sha256_digest]
+}
+
+resource "docker_volume" "transmission_config" {
+  name = "transmission_config"
+}
+
+resource "docker_volume" "transmission_watch" {
+  name = "transmission_watch"
+}
+
+resource "docker_container" "rproxy_container" {
+  name = "rproxy"
+  image = docker_image.rproxy_latest
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "domain_name" {
   type = string
   description = "Domain name for reverse proxy to use, e.g. example.com"
 }
+
+variable "docker_user" {
+  type = string
+  description = "User who has docker permissions on the remote host.  Passwordless SSH required."
+}
